078a31ac0545fd724ed3ad6532723348ccc6ce0f
Brian James <brian.james@slalom.com>
2018-08-07 13:44:03 -0700

    first commit

---
+++ src/containers/item-list.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+class ItemList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            items: [],
+            hasErrored: false,
+            isLoading: false
+        };
+    }
+
+    fetchData(url) {
+        this.setState({ isLoading: true });
+        fetch(url)
+            .then((response) => {
+                console.log(response)
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                this.setState({ isLoading: false });
+                return response;
+            })
+            .then((response) => response.json())
+            .then((items) => {
+                this.setState({ items }) // ES6 property value shorthand for { items: items }
+            })
+            .catch(() => this.setState({ hasErrored: true }));
+    }
+
+    componentDidMount() {
+        this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+    }
+
+    render() {
+        if (this.state.hasErrored) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+        if (this.state.isLoading) {
+            return <p>Loading…</p>;
+        }
+        return (
+            <ul>
+                {this.state.items.map((item) => (
+                    <li key={item.id}>
+                        {item.label}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+export default ItemList;
